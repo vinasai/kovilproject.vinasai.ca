f5ca06a7f8dc95c2989003be4e3ca53ff29573fc
aathubala <aathuofficial4522@gmail.com>
2025-04-01 20:05:37 +0530

    Mobile Responsiveness Issue fixed on 04/01/2025 by aatharsan

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -11,9 +11,9 @@ const HeroSection = () => {
       >
         <div className="absolute inset-0 bg-black opacity-50"></div> {/* Overlay */}
         <div className="relative text-left text-white p-10">
-          <h1 className="typing-effect text-5xl text-yellow-500 md:text-7xl lg:text-8xl font-bold mb-3">
-            Sri Varasiththi Vinaayagar
-          </h1>
+        <h1 className="typing-effect text-2xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl text-yellow-500 font-bold mb-3">
+          Sri Varasiththi Vinaayagar
+        </h1>
           <h1 className="text-4xl md:text-5xl lg:text-6xl font-semibold mb-3">
             Hindu Temple
           </h1>
